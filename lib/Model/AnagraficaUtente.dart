a725d0e714a54ecee3b949a8b0cf2d9f16b7ef60
Riccardo Pierini <riccardo.pierini@studenti.unicam.it>
2022-04-10 11:50:21 +0200

    Updating AnagraficaUtente.dart with json methods.

--- lib/Model/AnagraficaUtente.dart
+++ lib/Model/AnagraficaUtente.dart
@@ -1,30 +1,55 @@
 import 'dart:ffi';
 
 class AnagraficaUtente {
-  int _id = 0;
-  String _nomeUtente;
-  int _altezzaUtente;
-  Float _pesoUtente;
-  int _etaUtente;
+  int? _id = 0;
+  String? _nomeUtente;
+  int? _altezzaUtente;
+  Float? _pesoUtente;
+  DateTime? _dataNascitaUtente;
+  bool? _sesso;
 
   AnagraficaUtente(
-      this._altezzaUtente, this._etaUtente, this._nomeUtente, this._pesoUtente);
+      this._altezzaUtente, this._dataNascitaUtente, this._nomeUtente, this._pesoUtente, this._sesso);
 
-  int get id => _id;
+  AnagraficaUtente.fromJson(Map<String, dynamic> json) {
+    _id = json['id'];
+    _nomeUtente = json['nome'];
+    _sesso = json['sesso'];
+    _altezzaUtente = json['altezza'];
+    _dataNascitaUtente = json['dataNascita'];
+    _pesoUtente = json['peso'];
+  }
 
-  String get nomeUtente => this._nomeUtente;
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = _id;
+    data['nome'] = _nomeUtente;
+    data['sesso'] = _sesso;
+    data['altezza'] = _altezzaUtente;
+    data['dataNascita'] = _dataNascitaUtente;
+    data['peso'] = _pesoUtente;
+    return data;
+  }
 
-  set nomeUtente(String nome) => this._nomeUtente = nome;
+  int? get id => _id;
 
-  int get altezzaUtente => this._altezzaUtente;
+  String? get nomeUtente => this._nomeUtente;
 
-  set altezzaUtente(int altezza) => this._altezzaUtente = altezza;
+  set nomeUtente(String? nome) => this._nomeUtente = nome;
 
-  Float get pesoUtente => this._pesoUtente;
+  int? get altezzaUtente => this._altezzaUtente;
 
-  set pesoUtente(Float peso) => this._pesoUtente = peso;
+  set altezzaUtente(int? altezza) => this._altezzaUtente = altezza;
 
-  int get etaUtente => this._etaUtente;
+  Float? get pesoUtente => this._pesoUtente;
 
-  set etaUtente(int eta) => this._etaUtente = eta;
+  set pesoUtente(Float? peso) => this._pesoUtente = peso;
+
+  DateTime? get dataNascitaUtente => this._dataNascitaUtente;
+
+  set dataNascitaUtente(DateTime? eta) => this._dataNascitaUtente = eta;
+
+  bool? get sessoUtente => this._sesso;
+
+  set sessoUtente(bool? sesso) => this._sesso = sesso;
 }
