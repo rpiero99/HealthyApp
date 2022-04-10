8e179614f9c2f0a3166633fc1f5f4d6b82f647a7
alessandrotesta-99 <alessandro.testa@studenti.unicam.it>
2022-04-10 14:40:53 +0200

    add json settings

--- lib/Model/CronometroProgrammabile.dart
+++ lib/Model/CronometroProgrammabile.dart
@@ -1,37 +1,33 @@
 import 'dart:async';
 
 class CronometroProgrammabile {
-  int? _id;
   Timer? _timer;
   Timer? _tempoPreparazione;
   Timer? _tempoRiposo;
   Timer? _tempoLavoro;
   int? _tempoTotale;
+  int? _id;
 
   CronometroProgrammabile(this._tempoLavoro, this._tempoPreparazione,
       this._tempoRiposo, this._tempoTotale, this._timer);
 
   CronometroProgrammabile.fromJson(Map<String, dynamic> json) {
     _id = json['id'];
-    _timer = json['timer'];
-    _tempoLavoro = json['tempoWork'];
     _tempoPreparazione = json['tempoPreparazione'];
-    _tempoRiposo = json['tempoRiposo'];
     _tempoTotale = json['tempoTotale'];
+    _tempoLavoro = json['tempoWork'];
+    _timer = json['timer'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = _id;
-    data['timer'] = _timer;
-    data['tempoWork'] = _tempoLavoro;
     data['tempoPreparazione'] = _tempoPreparazione;
     data['tempoTotale'] = _tempoTotale;
-    data['tempoRiposo'] = _tempoRiposo;
+    data['tempoWork'] = _tempoLavoro;
+    data['timer'] = _timer;
     return data;
   }
-
-  int? get id => _id;
 
   Timer? get timer => this._timer;
 
@@ -53,4 +49,8 @@ class CronometroProgrammabile {
   int? get tempoTotale => this._tempoTotale;
 
   set tempoTotale(int? tempoTotale) => this._tempoTotale = tempoTotale;
+
+  void startTimer() {}
+
+  void stopTimer() {}
 }
