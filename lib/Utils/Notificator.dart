f50ff2c5583f8d977d6d6c34e2e47a80d1fadd15
alessandrotesta-99 <alessandro.testa@studenti.unicam.it>
2022-04-19 23:18:40 +0200

    add class notificator

---
+++ lib/Utils/Notificator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class Notificator{
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> init() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');
+
+    const InitializationSettings initializationSettings =
+     InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: null,
+        macOS: null);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+  }
+
+  void selectNotification(String? payload) {
+  }
+}
